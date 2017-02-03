1ab12694f97851d69a45ee053be2761b9a0d8ff4
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-03 15:04:37 -0500

    Added main panel to UI.

--- src/SubmissionInterface.py
+++ src/SubmissionInterface.py
@@ -9,50 +9,64 @@ import time
 
 # SubmissionInterface is used to submit assignments.
 class SubmissionInterface:
+
+	# UI elements.
+	screenMain = None
+	screenSize = (0, 0)
+	panelTop = None
+	panelInfo = None
+	panelTime = None
+
 	def __init__(self):
 		pass
 	
 	def show(self):
 		print("Showing Submission UI")
-		
 		try:
-			curses.wrapper(self.draw)
+			curses.wrapper(self._draw)
 		except Exception as err:
 			print("curseserror: " + str(err))
 	
-	def draw(self, stdscr):
+	def _draw(self, stdscr):
+		self.screenMain = stdscr
 		try:
 			# UI Setup
-			termSize = stdscr.getmaxyx()
+			self.screenSize = stdscr.getmaxyx()
 			curses.curs_set(0)
-			stdscr.hline(2, 0, curses.ACS_HLINE, termSize[1])
+			# stdscr.hline(2, 0, curses.ACS_HLINE, self.screenSize[1])
 			stdscr.refresh()
 			
 			# Top panel.
-			topPanel = curses.newwin(2, termSize[1], 0, 0)
+			self.panelTop = curses.newwin(2, self.screenSize[1], 0, 0)
 			
 			# Information panel on the left of the top panel.
-			infoPanel = topPanel.derwin(2, termSize[1] - 12, 0, 0)
-			infoPanel.addstr(0, 0, "ADVANCED SUBMISSION SYSTEM", curses.A_STANDOUT)
-			infoPanel.addstr(1, 3, "Purdue Computer Science", curses.A_DIM)
-			infoPanel.addstr(1, 35, "SUBMIT ASSIGNMENT", curses.A_NORMAL)
-			# infoPanel.addstr(1, 3, "Purdue Computer Science", curses.A_DIM)
-			infoPanel.refresh()
+			self.panelInfo = self.panelTop.derwin(2, self.screenSize[1] - 12, 0, 0)
+			self.panelInfo.addstr(0, 0, "ADVANCED SUBMISSION SYSTEM", curses.A_STANDOUT)
+			self.panelInfo.addstr(1, 3, "Purdue Computer Science", curses.A_DIM)
+			self.panelInfo.addstr(1, 35, "SUBMIT ASSIGNMENT", curses.A_NORMAL)
+			self.panelInfo.refresh()
 			
 			# Time panel in the top-right corner.
-			timePanel = topPanel.derwin(2, 12, 0, termSize[1] - 12)
-			timePanel.addstr(0, 0, "Current Time", curses.A_UNDERLINE)
+			self.panelTime = self.panelTop.derwin(2, 12, 0, self.screenSize[1] - 12)
+			self.panelTime.addstr(0, 0, "Current Time", curses.A_UNDERLINE)
+			
+			# Main panel.
+			self.panelMain = curses.newwin(self.screenSize[0] - 2, self.screenSize[1], 2, 0)
+			self.panelMain.box()
+			self.panelMain.refresh()
 
 			# UI Loop
 			while True:
-				# Update time panel.
-				timePanel.addstr(1, 0, time.strftime("%I:%M:%S %p"))
-				timePanel.refresh()
-
-				# stdscr.refresh()
-				# stdscr.getkey()
-				
+				self._drawUpdate()
 				# No need to refresh faster than 1 FPS for this example...
 				time.sleep(1)
+		except Exception as err:
+			raise err
+
+	def _drawUpdate(self):
+		try:
+			# Update time panel.
+			self.panelTime.addstr(1, 0, time.strftime("%I:%M:%S %p"))
+			self.panelTime.refresh()
 		except Exception as err:
 			raise err
