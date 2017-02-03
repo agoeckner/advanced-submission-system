e0a244d4ae685b09618e678de1e146c6a8a31de5
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-03 14:51:39 -0500

    Error handling and derived windows.

--- src/SubmissionInterface.py
+++ src/SubmissionInterface.py
@@ -15,33 +15,44 @@ class SubmissionInterface:
 	def show(self):
 		print("Showing Submission UI")
 		
-		curses.wrapper(self.draw)
+		try:
+			curses.wrapper(self.draw)
+		except Exception as err:
+			print("curseserror: " + str(err))
 	
 	def draw(self, stdscr):
-		# UI Setup
-		termSize = stdscr.getmaxyx()
-		curses.curs_set(0)
-		stdscr.hline(2, 0, curses.ACS_HLINE, termSize[1])
-		stdscr.refresh()
-		
-		# Information panel in the top-left corner.
-		infoPanel = curses.newwin(2, int(termSize[1] / 2), 0, 0)
-		infoPanel.addstr(0, 0, "ADVANCED SUBMISSION SYSTEM", curses.A_STANDOUT)
-		infoPanel.addstr(1, 0, "   Purdue Computer Science", curses.A_DIM)
-		infoPanel.refresh()
-		
-		# Time panel in the top-right corner.
-		timePanel = curses.newwin(2, 12, 0, termSize[1] - 12)
-		timePanel.addstr(0, 0, "Current Time", curses.A_UNDERLINE)
-
-		# UI Loop
-		while True:
-			# Update time panel.
-			timePanel.addstr(1, 0, time.strftime("%I:%M:%S %p"))
-			timePanel.refresh()
-
-			# stdscr.refresh()
-			# stdscr.getkey()
+		try:
+			# UI Setup
+			termSize = stdscr.getmaxyx()
+			curses.curs_set(0)
+			stdscr.hline(2, 0, curses.ACS_HLINE, termSize[1])
+			stdscr.refresh()
 			
-			# No need to refresh faster than 1 FPS for this example...
-			time.sleep(1)
+			# Top panel.
+			topPanel = curses.newwin(2, termSize[1], 0, 0)
+			
+			# Information panel on the left of the top panel.
+			infoPanel = topPanel.derwin(2, termSize[1] - 12, 0, 0)
+			infoPanel.addstr(0, 0, "ADVANCED SUBMISSION SYSTEM", curses.A_STANDOUT)
+			infoPanel.addstr(1, 3, "Purdue Computer Science", curses.A_DIM)
+			infoPanel.addstr(1, 35, "SUBMIT ASSIGNMENT", curses.A_NORMAL)
+			# infoPanel.addstr(1, 3, "Purdue Computer Science", curses.A_DIM)
+			infoPanel.refresh()
+			
+			# Time panel in the top-right corner.
+			timePanel = topPanel.derwin(2, 12, 0, termSize[1] - 12)
+			timePanel.addstr(0, 0, "Current Time", curses.A_UNDERLINE)
+
+			# UI Loop
+			while True:
+				# Update time panel.
+				timePanel.addstr(1, 0, time.strftime("%I:%M:%S %p"))
+				timePanel.refresh()
+
+				# stdscr.refresh()
+				# stdscr.getkey()
+				
+				# No need to refresh faster than 1 FPS for this example...
+				time.sleep(1)
+		except Exception as err:
+			raise err
