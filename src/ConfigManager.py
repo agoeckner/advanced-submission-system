8d26c9a7ea165565c7c0b0a393f70f74739186f8
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-04 17:13:43 -0500

    Changes spaces to tabs.

--- src/ConfigManager.py
+++ src/ConfigManager.py
@@ -53,51 +53,46 @@ class ConfigManager:
  
  
 	def get_config(path):
-	    """
-	    Returns the config object
-	    """
-	    if not os.path.exists(path):
-	        create_config(path)
+		"""
+		Returns the config object
+		"""
+		if not os.path.exists(path):
+			create_config(path)
 	 
-	    config = ConfigParser.ConfigParser()
-	    config.read(path)
-	    return config
+		config = ConfigParser.ConfigParser()
+		config.read(path)
+		return config
 	 
 	 
 	def get_setting(path, section, setting):
-	    """
-	    Print out a setting
-	    """
-	    config = get_config(path)
-	    value = config.get(section, setting)
-	    print "{section} {setting} is {value}".format(
-	        section=section, setting=setting, value=value)
-	    return value
-	 
-	 
+		"""
+		Print out a setting
+		"""
+		config = get_config(path)
+		value = config.get(section, setting)
+		print "{section} {setting} is {value}".format(
+			section=section, setting=setting, value=value)
+		return value
+	
+	
 	def update_setting(path, section, setting, value):
-	    """
-	    Update a setting
-	    """
-	    config = get_config(path)
-	    config.set(section, setting, value)
-	    with open(path, "wb") as config_file:
-	        config.write(config_file)
+		"""
+		Update a setting
+		"""
+		config = get_config(path)
+		config.set(section, setting, value)
+		with open(path, "wb") as config_file:
+			config.write(config_file)
 	 
 	 
 	def delete_setting(path, section, setting):
-	    """
-	    Delete a setting
-	    """
-	    config = get_config(path)
-	    config.remove_option(section, setting)
-	    with open(path, "wb") as config_file:
-	        config.write(config_file)
+		"""
+		Delete a setting
+		"""
+		config = get_config(path)
+		config.remove_option(section, setting)
+		with open(path, "wb") as config_file:
+			config.write(config_file)
 		
 if __name__ == '__main__':
-    main()
-
-
-
-
-	
+	main()
