3cef20cb2daefd1ff337557affaaa59f1e5a8ff3
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-06 21:27:21 -0500

    InputManager is now functional. Allows for tabbing between elements and directing input to the selected element.

--- src/InputManager.py
+++ src/InputManager.py
@@ -4,29 +4,47 @@
 # Purdue University
 #=================================
 
+import curses
+
 # InputManager handles tabbing between elements in a UI, determining which receives user input.
 class InputManager:
 	parent = None
+	elements = []
+	currentIdx = 0
+	currentElement = None
 	def __init__(self, parent):
 		self.parent = parent
 	
 	# Adds a UI element to the manager.
-	def addElement(self):
-		#TODO
-		pass
+	def addElement(self, element):
+		self.elements.append(element)
+		if len(self.elements) == 1:
+			self.setSelectedIndex(0)
+		else:
+			element.onLoseFocus()
 	
 	def tabNext(self):
-		#TODO
-		pass
-	
+		nextIdx = (self.currentIdx + 1) % len(self.elements)
+		self.setSelectedIndex(nextIdx)
+		return self.currentElement
+		
 	def tabPrev(self):
-		#TODO
-		pass
+		prevIdx = (self.currentIdx - 1) % len(self.elements)
+		self.setSelectedIndex(prevIdx)
+		return self.currentElement
 	
-	def setSelected(self, element):
-		#TODO
-		pass
+	def setSelectedIndex(self, idx):
+		if self.currentElement is not None:
+			self.currentElement.onLoseFocus()
+		self.currentIdx = idx
+		self.currentElement = self.elements[idx]
+		self.currentElement.onFocus()
 	
 	def onInput(self, inputChar):
-		#TODO
-		pass
+		if inputChar == ord('\t') or inputChar == curses.KEY_ENTER:
+			self.tabNext()
+			return None
+		elif inputChar == curses.KEY_BACKSPACE:
+			self.tabPrev()
+			return None
+		return self.currentElement.onInput(inputChar)
