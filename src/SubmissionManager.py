002663c79f7b9594ac0a229007812d00df8f176c
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-07 11:25:36 -0500

    Added SubmissionManager::submitAssignment stub.

--- src/SubmissionManager.py
+++ src/SubmissionManager.py
@@ -15,3 +15,10 @@ class SubmissionManager:
 		# TODO: Actually write this function!
 		return ["cs180", "cs240", "cs250", "cs251", "cs252", "cs354", "cs307",
 			"cs422", "cs408"]
+	
+	# Submits an assignment.
+	# course, assignment are strings
+	# files is a list of file strings
+	# Return true on success, false on failure
+	def submitAssignment(self, course, assignment, files):
+		return False
