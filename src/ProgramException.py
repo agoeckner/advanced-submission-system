5d7096f9e4d516ef1d254b79902dc6ec94759ac3
Anthony Goeckner <agoeckne@purdue.edu>
2017-02-27 23:12:17 -0500

    Place all exceptions in a common file.

---
+++ src/ProgramException.py
@@ -0,0 +1,8 @@
+#=================================
+# Advanced Submission System
+# CS 40800 - Software Engineering
+# Purdue University
+#=================================
+
+class MissingCallback(Exception):
+	pass
