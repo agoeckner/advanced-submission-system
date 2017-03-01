a68f5d629532960e99de8c23142f24f9f609ba7a
Anthony Goeckner <agoeckne@purdue.edu>
2017-03-01 17:21:55 -0500

    Create separate applications called "grades" and "submit".

---
+++ src/initSubmit.py
@@ -0,0 +1,34 @@
+#=================================
+# Advanced Submission System
+# CS 40800 - Software Engineering
+# Purdue University
+#=================================
+
+import os
+import sys
+import SubmissionInterface
+import SubmissionManager
+import CourseManager
+
+GLOBAL_PATH = "/etc/submission/global.config"
+
+class AdvancedSubmissionSystem:
+	submissionManager = None
+	submissionUI = None
+	courseManager = None
+	
+	# Perform program initialization.
+	def __init__(self):
+		# Fix for ncurses over PuTTY.
+		os.environ["NCURSES_NO_UTF8_ACS"] = "1"
+		
+		self.submissionManager = SubmissionManager.SubmissionManager(self)
+		self.submissionUI = SubmissionInterface.SubmissionInterface(self)
+		try:
+			self.submissionUI.show()
+		except KeyboardInterrupt:
+			print("WARNING: Nothing was submitted!")
+
+# Start the program.
+if __name__ == '__main__':
+	AdvancedSubmissionSystem()
