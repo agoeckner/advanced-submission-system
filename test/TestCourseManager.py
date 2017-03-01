48047daba6b1a2dfd240ed96424d23c05d46b121
smithhe <smithhe@purdue.edu>
2017-03-01 15:49:34 -0500

    Created test class for course manager

---
+++ test/TestCourseManager.py
@@ -0,0 +1,14 @@
+#=================================
+# Advanced Submission System
+# CS 40800 - Software Engineering
+# Purdue University
+#=================================
+
+import sys
+sys.path.append('../src/')
+import unittest
+import curses
+
+class TestCourseManager: #{
+
+}
