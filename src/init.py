cb8ef4f78260f711a09b904e5f2c042185d81c8c
Anthony Goeckner <agoeckne@purdue.edu>
2017-03-01 12:27:22 -0500

    Only launch the "submit" interface via init.py.

--- src/init.py
+++ src/init.py
@@ -23,27 +23,12 @@ class AdvancedSubmissionSystem:
 		# Fix for ncurses over PuTTY.
 		os.environ["NCURSES_NO_UTF8_ACS"] = "1"
 		
-		if len(sys.argv) > 1 and sys.argv[1] == "grades":
-			# Setup
-			self.submissionManager = SubmissionManager.SubmissionManager(self)
-			self.studentGradeUI = StudentInterface.StudentInterface(self)
-			try:
-				self.studentGradeUI.show()
-			except KeyboardInterrupt:
-				print("Goodbye!")
-		elif len(sys.argv) > 1 and sys.argv[1] == "course":
-			#Setup
-			self.courseManager = CourseManager.CourseManager(self)
-			
-			self.courseManager.start()
-		else:
-			# Setup
-			self.submissionManager = SubmissionManager.SubmissionManager(self)
-			self.submissionUI = SubmissionInterface.SubmissionInterface(self)
-			try:
-				self.submissionUI.show()
-			except KeyboardInterrupt:
-				print("WARNING: Nothing was submitted!")
+		self.submissionManager = SubmissionManager.SubmissionManager(self)
+		self.submissionUI = SubmissionInterface.SubmissionInterface(self)
+		try:
+			self.submissionUI.show()
+		except KeyboardInterrupt:
+			print("WARNING: Nothing was submitted!")
 
 # Start the program.
 if __name__ == '__main__':
